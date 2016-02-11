bfc70c663706ccdefc7fd58b38036e60bbbfa600
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-02-11 12:00:06 -0800

    Add missing file

---
+++ index.js
@@ -0,0 +1,15 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2016 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+
+const Client = require('./lib/client/client');
+const Server = require('./lib/server/server');
+
+module.exports = {
+    Client: Client,
+    Server: Server
+};
